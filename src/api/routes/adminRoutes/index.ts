5e0181bf327d7ad532e478969f37b10b34873201
tirath-dikonia <tirath.sharma@dikonia.in>
2024-07-30 18:43:15 +0530

    added task

--- src/api/routes/adminRoutes/index.ts
+++ src/api/routes/adminRoutes/index.ts
@@ -2,7 +2,10 @@ import express from "express";
 const router = express.Router();
 import userRotes from './user.routes';
 import roleRotes from "./role.routes";
+import taskTypeRoutes from "./taskType.routes";
 
 router.use('/user', userRotes )
 router.use('/role', roleRotes)
+router.use('/task-type', taskTypeRoutes)
+
 export default router;
